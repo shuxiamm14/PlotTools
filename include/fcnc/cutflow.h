4041f8273137ae09cafbcd6ab930c641bc2a18c6
Boyang Li <boyang.li@cern.ch>
2020-03-27 17:17:46 +0800

    save cutflow to histogram

--- include/fcnc/cutflow.h
+++ include/fcnc/cutflow.h
@@ -4,33 +4,36 @@
 #include <vector>
 #include <iostream>
 #include "TString.h"
-class cutflow
+class CutFlow
 {
 public:
-	cutflow();
-	~cutflow();
+	CutFlow(TString _sample = "", TString _region = "");
+	~CutFlow();
 
 	Float_t* fweight;
 	Double_t* dweight;
-	ULong64_t* eventnumber;
-	bool iftrack;
-	int weight_type;
-	int ievttrack;
-	std::vector<std::vector<ULong64_t>> eventtrack;
+	ULong64_t* event_number;
+	TString sample;
+	TString region;
+	std::vector<std::vector<ULong64_t>> event_track;
 	int n_tracked_event;
-	std::vector<long> cutflowraw;
-	std::vector<double> cutflowweighted;
+	int n_cuts;
+	int i_cut;
+	bool if_track;
+	int weight_type;
+	int ievt_track;
+	std::vector<long> cutflow_raw;
+	std::vector<double> cutflow_weighted;
 	std::vector<double> cutflow2;
-	int nCuts;
-	int iCut;
-	std::vector<TString> cutname;
-	void trackevent(ULong64_t evtnumber);
-	void set_event_number(ULong64_t* _eventnumber){ eventnumber = _eventnumber; }
-	void set_weight(Float_t* _weight){ fweight = _weight; weight_type = 1;}
-	void set_weight(Double_t* _weight){ dweight = _weight; weight_type = 2;}
+	std::vector<TString> cut_names;
+	void trackEvent(ULong64_t evt_number);
+	void setEventNumber(ULong64_t* _event_number){ event_number = _event_number; }
+	void setWeight(Float_t* _weight){ fweight = _weight; weight_type = 1;}
+	void setWeight(Double_t* _weight){ dweight = _weight; weight_type = 2;}
 	void clear();
 	void newEvent();
 	void print();
-	void fill();
+	void save(int total_cuts);
+	void fill(TString cut_name = "");
 };
 #endif
--- src/fcnc/cutflow.cc
+++ src/fcnc/cutflow.cc
@@ -1,86 +1,124 @@
 #include "cutflow.h"
-
-cutflow::cutflow(){
-	nCuts = 0;
-	iCut = 0;
-	fweight = NULL;
-	dweight = NULL;
-
+#include "TFile.h"
+#include "TH1D.h"
+#include "TAxis.h"
+#include "fcnc_include.h"
+CutFlow::CutFlow(TString _sample, TString _region) :
+sample(_sample), region(_region),
+n_tracked_event(0), n_cuts(0), i_cut(0), if_track(0), weight_type(0), ievt_track(0),
+fweight(NULL), dweight(NULL), event_number(NULL)
+{
 }
 
 
-cutflow::~cutflow(){
+CutFlow::~CutFlow(){
 }
 
-void cutflow::clear(){
-	for(auto &iter : cutflowraw) iter = 0;
-	for(auto &iter : cutflowweighted) iter = 0;
+void CutFlow::clear(){
+	for(auto &iter : cutflow_raw) iter = 0;
+	for(auto &iter : cutflow_weighted) iter = 0;
 	for(auto &iter : cutflow2) iter = 0;
 	if(n_tracked_event) {
-		std::vector<ULong64_t> tmp = eventtrack[0];
-		eventtrack.clear();
-		eventtrack[0] = tmp;
+		std::vector<ULong64_t> tmp = event_track[0];
+		event_track.clear();
+		event_track[0] = tmp;
 	}
-	iCut = 0;
+	i_cut = 0;
 }
 
-void cutflow::newEvent(){
-	iftrack = 0;
+void CutFlow::newEvent(){
+	if_track = 0;
 	if(n_tracked_event)
-		for(auto evt: eventtrack[0]){
-			if(*eventnumber == evt)
-				iftrack = 1;
+		for(auto evt: event_track[0]){
+			if(*event_number == evt)
+				if_track = 1;
 		}
-	iCut = 0;
+	i_cut = 0;
 }
 
-void cutflow::trackevent(ULong64_t evtnumber) {
+void CutFlow::trackEvent(ULong64_t evtnumber) {
 	n_tracked_event++;
 	if(n_tracked_event == 1)
-		eventtrack.push_back(std::vector<ULong64_t>());
-	eventtrack[0].push_back(evtnumber);
+		event_track.push_back(std::vector<ULong64_t>());
+	event_track[0].push_back(evtnumber);
 
 }
 
 
-void cutflow::fill(){
+void CutFlow::fill(TString cut_name){
 	double weight = weight_type == 1? *fweight : *dweight;
-	if(cutflowraw.size() <= iCut) {
-		cutflowraw.push_back(1);
-		cutflowweighted.push_back(weight);
+	if(cutflow_raw.size() <= i_cut) {
+		cut_names.push_back(cut_name);
+		cutflow_raw.push_back(1);
+		cutflow_weighted.push_back(weight);
 		cutflow2.push_back(weight*weight);
-		nCuts ++;
+		n_cuts ++;
 	}else{
-		cutflowraw[iCut] += 1;
-		cutflowweighted[iCut] += weight;
-		cutflow2[iCut] += weight*weight;
+		cutflow_raw[i_cut] += 1;
+		cutflow_weighted[i_cut] += weight;
+		cutflow2[i_cut] += weight*weight;
 	}
-	if(iftrack){
-		if(eventtrack.size() <= iCut+1) eventtrack.push_back(std::vector<ULong64_t>());
-		eventtrack[iCut+1].push_back(*eventnumber);
+	if(if_track){
+		if(event_track.size() <= i_cut+1) event_track.push_back(std::vector<ULong64_t>());
+		event_track[i_cut+1].push_back(*event_number);
 	}
-	iCut ++;
+	i_cut ++;
 }
 
-void cutflow::print(){
-	printf("cutflow:");
-	if(nCuts == 0) {
-		printf("no Cut applied\n");
+void CutFlow::save(int total_cuts){
+	if(n_cuts == 0) {
+		printf("CutFlow::save() : WARNING : no Cut applied, nothing saved\n");
 		return;
 	}
-	for (int i = 0; i < nCuts-1; ++i)
+	int nbins(n_cuts);
+	if(total_cuts > n_cuts) nbins = total_cuts;
+	TFile *save_file = new TFile("cutflow_" + sample + ".root", "update");
+	TH1D *save_hist = (TH1D*)save_file->Get(region);
+	if(!save_hist) save_hist = new TH1D(region,region,nbins,0,nbins);
+	TAxis *xaxis = save_hist->GetXaxis();
+	for (int i = 0; i < nbins; ++i)
 	{
-		printf(" %f +/- %f,", cutflowweighted[i], sqrt(cutflow2[i]));
+		if(xaxis->GetBinLabel(i) == cut_names[0] || xaxis->GetBinLabel(i) == TString("")){
+			for (int j = 0; j < n_cuts; ++j)
+			{
+				save_hist->SetBinContent(i+j,cutflow_weighted[j]);
+				save_hist->SetBinError(i+j,sqrt(cutflow2[j]));
+				xaxis->SetBinLabel(i+j,cut_names[j]);
+			}
+			break;
+		}
 	}
-	printf(" %f +/- %f\n", cutflowweighted[nCuts-1], sqrt(cutflow2[nCuts-1]));
-	printf("cutflowraw:");
-	for (int i = 0; i < nCuts-1; ++i)
+	save_file->cd();
+	save_hist->Write(region,TObject::kWriteDelete);
+	save_file->Close();
+	deletepointer(save_file);
+}
+
+void CutFlow::print(){
+	printf("cutflow:");
+	if(n_cuts == 0) {
+		printf("CutFlow::print() : no Cut applied\n");
+		return;
+	}
+
+	for (int i = 0; i < n_cuts; ++i)
 	{
-		printf(" %ld,", cutflowraw[i]);
+		printf("%s", cut_names[n_cuts].Data());
 	}
-	printf(" %ld\n", cutflowraw[nCuts-1]);
+	printf("\n");
+	for (int i = 0; i < n_cuts; ++i)
+	{
+		printf(" %f+/-%f", cutflow_weighted[i], sqrt(cutflow2[i]));
+	}
+	printf("\n");
+	printf("cutflow_raw:");
+	for (int i = 0; i < n_cuts; ++i)
+	{
+		printf(" %ld", cutflow_raw[i]);
+	}
+	printf("\n");
 	if(n_tracked_event){
-		for(auto cut: eventtrack){
+		for(auto cut: event_track){
 			printf("cut: ");
 			for(auto evt : cut)
 				printf("%llu,",evt);
